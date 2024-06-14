1c827c627dded35f4db3f6d0842d614d71cb87dc
ForwardGT <ollmaincore.gmail.com>
2024-06-15 02:54:34 +0300

    fix RocketLaunchScreen

--- app/src/main/java/com/example/spacexmobileapp/presentation/firstLaunch/RocketLaunchScreen.kt
+++ app/src/main/java/com/example/spacexmobileapp/presentation/firstLaunch/RocketLaunchScreen.kt
@@ -1,4 +1,4 @@
-package com.example.spacexmobileapp
+package com.example.spacexmobileapp.presentation.firstLaunch
 
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.LinearEasing
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,12 +19,14 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import com.example.spacexmobileapp.R
 import kotlinx.coroutines.launch
 
 @Composable
@@ -32,44 +35,58 @@ fun RocketLaunchScreen() {
     val screenHeight = configuration.screenHeightDp.dp
     val screenWidth = configuration.screenWidthDp.dp
 
-    val screenHeightPx = with(LocalDensity.current) { screenHeight.toPx() }
+    val screenHeightPx = with(LocalDensity.current) { screenHeight.toPx() / 6 }
     val screenWidthPx = with(LocalDensity.current) { screenWidth.toPx() }
 
     val rocketOffsetY = remember { Animatable(screenHeightPx) }
     val coroutineScope = rememberCoroutineScope()
     var isLaunched by remember { mutableStateOf(false) }
 
-    val imageSize = 1000.dp
+    val imageSize = 900.dp
 
     fun launchRocket() {
         coroutineScope.launch {
             rocketOffsetY.snapTo(screenHeightPx)
             rocketOffsetY.animateTo(
                 targetValue = (-imageSize * 3).value,
-                animationSpec = tween(durationMillis = 2500, easing = LinearEasing)
+                animationSpec = tween(durationMillis = 2000, easing = LinearEasing)
             )
         }
     }
 
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
+    Box(modifier = Modifier.fillMaxSize()) {
         Image(
-            painter = painterResource(id = R.drawable.rocket),
-            contentDescription = "Rocket",
-            modifier = Modifier
-                .size(imageSize)
-                .offset {
-                    IntOffset(
-                        ((-screenWidthPx / 2) + (screenWidth / 2).toPx()).toInt(),
-                        rocketOffsetY.value.toInt()
-                    )
-                },
-            contentScale = ContentScale.FillHeight
+            painter = painterResource(id = R.drawable.background_rocket_screen),
+            contentDescription = "Background",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
         )
-        Button(onClick = { isLaunched = true; launchRocket() }) {
-            Text("Launch")
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.rocket),
+                contentDescription = "Rocket",
+                modifier = Modifier
+                    .size(imageSize)
+                    .offset {
+                        IntOffset(
+                            ((-screenWidthPx / 2) + (screenWidth / 2).toPx()).toInt(),
+                            (rocketOffsetY.value.toInt() + rocketOffsetY.value.toInt() / 8)
+                        )
+                    },
+                contentScale = ContentScale.FillHeight
+            )
+            Button(
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.DarkGray,
+                    contentColor = Color.White
+                ),
+                onClick = { isLaunched = true; launchRocket() }
+            ) {
+                Text("Launch")
+            }
         }
     }
 }
