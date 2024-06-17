ce6f6939b69b5e1fe341003d4393a2baf2d02d54
ForwardGT <ollmaincore.gmail.com>
2024-06-18 02:18:36 +0300

    add SwitcherTheme and fix SpacexLogoHeader

--- app/src/main/java/com/example/spacexmobileapp/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/spacexmobileapp/presentation/main/MainActivity.kt
@@ -6,6 +6,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.example.spacexmobileapp.ui.theme.SpaceXMobileAppTheme
 
 
@@ -16,8 +20,15 @@ class MainActivity : ComponentActivity() {
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         enableEdgeToEdge()
         setContent {
-            SpaceXMobileAppTheme {
-                CarouselSlider()
+            var darkThemeActivate by remember { mutableStateOf(false) }
+
+            SpaceXMobileAppTheme(
+                darkTheme = darkThemeActivate
+            ) {
+                MainScreen(
+                    onUpdatedTheme = { darkThemeActivate = !darkThemeActivate },
+                    darkThemeActivate
+                )
             }
         }
     }
--- app/src/main/java/com/example/spacexmobileapp/presentation/main/MainScreen.kt
+++ app/src/main/java/com/example/spacexmobileapp/presentation/main/MainScreen.kt
@@ -1,54 +1,128 @@
 package com.example.spacexmobileapp.presentation.main
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.spacexmobileapp.R
+import com.example.spacexmobileapp.preview.PreviewFun
+import com.example.spacexmobileapp.ui.theme.Purple100
 
 @Composable
-fun MainScreen() {
-    Scaffold(
-        topBar = {
-            SpacexLogoHeader()
+fun MainScreen(
+    onUpdatedTheme: () -> Unit,
+    darkTheme: Boolean
+) {
+    Scaffold { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+        ) {
+            SpacexLogoHeader(
+                darkTheme = darkTheme,
+                clickListener = onUpdatedTheme
+            )
+            Spacer(modifier = Modifier.padding(top = 10.dp))
+            CarouselSlider()
+            Spacer(modifier = Modifier.padding(top = 10.dp))
+            Box {
+                Text(
+                    textAlign = TextAlign.Center,
+                    text = stringResource(R.string.description_company_spacex)
+                )
+            }
         }
-    ) { paddingValues ->
-        Box(modifier = Modifier.padding(paddingValues))
     }
 }
 
+
 @Composable
-private fun SpacexLogoHeader() {
+private fun SpacexLogoHeader(
+    clickListener: () -> Unit,
+    darkTheme: Boolean
+) {
     Row(
-        horizontalArrangement = Arrangement.Absolute.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier.fillMaxWidth()
     ) {
-        Image(
-            modifier = Modifier.size(width = 200.dp, height = 30.dp),
-            painter = painterResource(id = R.drawable.spacex_mobile),
-            contentDescription = "spaceX",
-            contentScale = ContentScale.FillWidth
-        )
+        Row(
+            modifier = Modifier.weight(0.7f)
+        ) {
+            Image(
+                modifier = Modifier.size(width = 150.dp, height = 30.dp),
+                painter = painterResource(id = R.drawable.spacex_mobile),
+                contentDescription = "spaceX",
+                contentScale = ContentScale.FillWidth
+            )
+            Text(
+                modifier = Modifier.padding(top = 5.dp),
+                fontSize = 19.sp,
+                text = "Mobile app",
+                style = TextStyle(fontFamily = FontFamily.Cursive),
+                fontWeight = FontWeight.SemiBold
+            )
+        }
+        Box(
+            modifier = Modifier.weight(0.3f),
+            contentAlignment = Alignment.CenterEnd
+        ) {
+            SwitcherTheme(
+                onClick = clickListener,
+                darkTheme = darkTheme
+            )
+        }
+
+    }
+}
+
+@Composable
+private fun SwitcherTheme(
+    darkTheme: Boolean,
+    onClick: () -> Unit
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         Text(
-            modifier = Modifier.padding(top = 3.dp),
-            fontSize = 25.sp,
-            text = "Mobile app",
+            modifier = Modifier.padding(top = 5.dp),
             style = TextStyle(fontFamily = FontFamily.Cursive),
-            fontWeight = FontWeight.SemiBold
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 19.sp,
+            text = if (darkTheme) "Dark " else "Light"
+        )
+        Switch(
+            modifier = Modifier
+                .scale(0.8f)
+                .padding(top = 5.dp),
+            checked = darkTheme,
+            onCheckedChange = { onClick() },
+            colors = SwitchDefaults.colors(
+                checkedThumbColor = Purple100,
+            )
         )
     }
 }
+
+
